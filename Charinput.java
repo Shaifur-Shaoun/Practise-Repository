3971f3f437a56ba7129ae310821f8b25772889c6
Shaifur-Shaoun <shaounnsu241@gmail.com>
2025-03-09 11:53:08 +0600

    JavaCharacter

---
+++ Charinput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Charinput {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+char c;
+c = s.next().charAt(5);
+System.out.print(c);
+}
+}
