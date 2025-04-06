4fe8ca3cb9fa31f3371015e8dcf58d290357791c
Shaifur-Shaoun <shaounnsu241@gmail.com>
2025-04-07 00:57:29 +0600

    Class & objects

--- circle.java
+++ circle.java
@@ -3,20 +3,36 @@ import java.util.*;
 public class circle {
     private double radius, area;
     private String color;
+    public static int count;
 
     public circle(){
-        radius=1.0;
-        color="red";
+        // radius=1.0;
+        // color="red";
+        // count++;
+        this(1);//Constructor Chaining
     }
     public circle(double r){
-        radius=r;
-        color="red";
+        // radius=r;
+        // color="red";
+        // count++;
+        this(r,"Red");//Constructor Chaining
         }   
     public circle(double r,String c){
-        radius=r;
-        color=c;
+       radius=r;
+       color=c;
+
+        count++;
         
         
+    }
+    public circle(circle another){
+        this.radius=another.radius;
+        this.color=another.color;
+        count++;
+
+    }
+    public static int getCount(){
+        return count;
     }
     
     public double getRadius(){
@@ -44,12 +60,16 @@ public class circle {
         System.out.println(c2.getArea());
         
         circle c1=new circle(2.0,"blue");
+
+        circle c5=new circle();
         
         System.out.println(c1.getRadius());
         System.out.println(c1.getColor());
         System.out.println(c1.getArea());
+
+        System.out.println("Value of Count : " + circle.count);
         
-        
+        System.out.println("Method to Static : " + c1.getCount());
     }
 }
 
